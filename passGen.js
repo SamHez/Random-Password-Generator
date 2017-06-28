f8acf79a1ebd9e3e4223d54621dcc607860bcfde
James George <jamesgeorge998001@gmail.com>
2017-06-28 20:06:12 +0530

    Add files via upload

---
+++ passGen.js
@@ -0,0 +1,14 @@
+function generate(len)
+{
+var keys="abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*<>?/:~";
+var text="";
+for(var i=0;i<len;i++)
+{
+  text+=keys.charAt(Math.floor(Math.random()*keys.length));
+}
+return text;
+}
+function populate(size)
+{
+  document.myform.gen.value=generate(size);
+}
